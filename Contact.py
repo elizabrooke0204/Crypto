ed405ec93ca953a7cdf5cd1749947bfeef82d77e
BrockBasil <bbasil@purdue.edu>
2022-05-23 05:53:53 -0400

    Fix error where sender email was not being used

--- Contact.py
+++ Contact.py
@@ -5,10 +5,10 @@ from auth_cred import (gmail_account, gmail_password, phone_number)
 
 def send_msg(msg):
 	try:
-		server = smtplib.SMTP( "smtp.gmail.com", 587 )
+		server = smtplib.SMTP("smtp.gmail.com", 587)
 		server.starttls()
 		server.login(gmail_account, gmail_password)
-		server.sendmail("LeavingSnail", phone_number, msg)
+		server.sendmail(gmail_account, phone_number, msg)
 		server.quit()
 	except Exception as err:
 		print("COULD NOT SEND NOTIFICATION MESSAGE")
